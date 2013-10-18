eae6239450d4d588e4af31d742c9847b80e65445
Jason Lewis <jason@decomplecting.org>
2013-10-18 16:26:16 -0400

    Add imgs, generate-startup fn

--- src/eduwampus/routes/home.clj
+++ src/eduwampus/routes/home.clj
@@ -1,7 +1,8 @@
 (ns eduwampus.routes.home
   (:use compojure.core)
   (:require [eduwampus.views.layout :as layout]
-            [eduwampus.util :as util]))
+            [eduwampus.util :as util]
+            [edufaker.company :as fake]))
 
 (defn home-page []
   (layout/render
@@ -10,6 +11,11 @@
 (defn about-page []
   (layout/render "about.html"))
 
+(defn generate-startup []
+  {:name (fake/startup) :catchphrase (fake/catch-phrase)})
+
 (defroutes home-routes
   (GET "/" [] (home-page))
   (GET "/about" [] (about-page)))
+
+(generate-startup)
